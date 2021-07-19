d280e725fa8110199b9ca5901459bdcd57f92c85
LIM QING YUAN <TP046490@mail.apu.edu.my>
2021-07-19 16:01:20 +0800

    Add project files.

---
+++ Piscesco/Areas/Identity/Data/PiscescoUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Piscesco.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the PiscescoUser class
+    public class PiscescoUser : IdentityUser
+    {
+    }
+}
