2e88fe135dd02903cac432f3d5f37015370ff78f
LIM QING YUAN <TP046490@mail.apu.edu.my>
2021-07-29 00:44:46 +0800

    Seller View Order

--- Piscesco/Controllers/OrdersController.cs
+++ Piscesco/Controllers/OrdersController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Piscesco.Areas.Identity.Data;
 using Piscesco.Data;
 using Piscesco.Models;
 
@@ -13,16 +16,43 @@ namespace Piscesco.Controllers
     public class OrdersController : Controller
     {
         private readonly PiscescoModelContext _context;
+        private readonly UserManager<PiscescoUser> _userManager;
+        private static int _stallID;
 
-        public OrdersController(PiscescoModelContext context)
+        public OrdersController(PiscescoModelContext context, UserManager<PiscescoUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
+
         // GET: Orders
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> OrderByStall(int? id)
         {
-            return View(await _context.Order.ToListAsync());
+
+            if (id.HasValue)
+            {
+                //Debug.WriteLine(id);
+                _stallID = (int)id;
+            }
+            Debug.WriteLine(_stallID);
+            var products = from p in _context.Product select p;
+            products = products.Where(item => item.StallID.Equals(_stallID));
+            
+            ViewData["Products"] = products;
+
+            var orderList = await _context.Order.Where(orderItem => orderItem.StallID == _stallID && orderItem.Status == "Placed").ToListAsync();
+
+            return View(orderList);
+        }
+
+        public async Task<IActionResult> StallList()
+        {
+            var loginSession = await _userManager.FindByIdAsync(_userManager.GetUserId(User));
+            var stalls = from s in _context.Stall select s;
+            stalls = stalls.Where(item => item.OwnerID.Equals(loginSession.Id));
+
+            return View(await stalls.ToListAsync());
         }
 
         // GET: Orders/Details/5
@@ -145,6 +175,16 @@ namespace Piscesco.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+
+        public Uri GetImageByProductID(int productid)
+        {
+            BlobsController bc = new BlobsController();
+            var products = from p in _context.Product select p;
+            Product product = products.FirstOrDefault(item => item.ProductID == productid);
+            return bc.GetImage(product.ProductImage);
+
+        }
+
 
         private bool OrderExists(Guid id)
         {
